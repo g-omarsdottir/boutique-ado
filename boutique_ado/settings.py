4b573d1f100b3ca7fa70646ff3f6db213d80c0b5
Gudrun Omarsdottir <g.omarsdottir@gmail.com>
2024-05-30 17:31:29 +0000

    Add app home, add templates, urls, views, settings

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount', # Optional -- requires install using `django-allauth[socialaccount]`.
     # Apps
+    'home',
     # Third-party apps
     'django_extensions',
 ]
@@ -64,7 +65,10 @@ ROOT_URLCONF = 'boutique_ado.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
