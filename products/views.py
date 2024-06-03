6214d1f9aea5094e75a7e7f982526b696f564663
Gudrun Omarsdottir <g.omarsdottir@gmail.com>
2024-06-03 10:18:21 +0000

    Add product details functionality

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
@@ -14,3 +14,17 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    # Return from datbase - here no filtering, returning all products
+    product = get_object_or_404(Product, pk=product_id)
+
+    # Add products to the context, so the products will be available in the template
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
