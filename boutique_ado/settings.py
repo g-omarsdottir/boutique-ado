387928869ff931fe7fd3f819ecfc7274b8b758bb
Gudrun Omarsdottir <g.omarsdottir@gmail.com>
2024-06-10 08:41:52 +0000

    Add webhook handlers, fix base.html, add env var wh secret

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -183,11 +183,9 @@ STRIPE_CURRENCY = "usd"
 # video: STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 # video: STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 # From Guillermo Brachetta:
-STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY")
-STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
-
-
-
+STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY", "")
+STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY", "")
+STRIPE_WH_SECRET = os.getenv("STRIPE_WH_SECRET", "")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
