78e3c2789530f90e8102e978cfd2cf87a2a87f18
Gudrun Omarsdottir <g.omarsdottir@gmail.com>
2024-06-13 12:26:45 +0000

    Add to allowed hosts

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -38,7 +38,9 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "localhost",
     "8000-gomarsdotti-boutiqueado-30mi5yoqciv.ws.codeinstitute-ide.net",
+    ".ecommerce-boutique-ado-4d033b69ab97.herokuapp.com",
     "ecommerce-boutique-ado-4d033b69ab97.herokuapp.com",
+    "https://ecommerce-boutique-ado-4d033b69ab97.herokuapp.com",
     ".herokuapp.com",
 ]
 
